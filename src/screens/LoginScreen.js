b8068223ae271ff59fb11a0a6c1350c8f90b1722
Jack Allcock <jackallcock@yahoo.co.uk>
2019-02-15 20:33:14 +0000

    Add signup link and use inputs not social

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
-import { SocialIcon } from 'react-native-elements';
+import { View, Text, StyleSheet, ImageBackground, Alert} from 'react-native';
 import {Header} from "react-native-elements";
 import Colors from "../config/Colors";
 import Service from "../utils/Service";
-import * as Alert from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button} from 'react-native-elements';
 
 class LoginScreen extends React.Component {
 
@@ -34,7 +34,9 @@ class LoginScreen extends React.Component {
     render() {
         return (
             <ImageBackground
-                source={{uri: 'https://i.ibb.co/MnqwfYS/background2.png'}}
+                // source={{uri: 'https://cdn.wallpapersafari.com/71/75/rO3vmn.jpg'}}
+                // source={{uri: 'https://ak7.picdn.net/shutterstock/videos/11927447/thumb/1.jpg'}}
+                source={{uri: 'https://www.pixelstalk.net/wp-content/uploads/images2/Minimalist-Abstract_arrows-wallpaper-2560x1600.jpg'}}
                 style={styles.container}
             >
                 <Header style={{borderBottomWidth: 0}}
@@ -51,25 +53,57 @@ class LoginScreen extends React.Component {
                         }}
                 />
 
-                    <View style={styles.content}>
+                <View style={styles.inputs}>
 
-                        <Text style={styles.loginText}>Please login</Text>
+                    <Button
+                        containerStyle={styles.signUpContainer}
+                        buttonStyle={styles.signUpButton}
+                        iconContainerStyle={styles.icon}
+                        raised
+                        icon={
+                            <Icon
+                                style={styles.icon}
+                                name="user-plus"
+                                size={15}
+                                color="white"
+                            />
+                        }
+                        title='Create account' />
 
-                        <SocialIcon
-                            title='Sign In With Facebook'
-                            button
-                            raised={true}
-                            style={styles.button}
-                            type='facebook'
-                        />
-                        <SocialIcon
-                            title='Sign In With Google'
-                            button
-                            raised={true}
-                            style={styles.googleButton}
-                            type='google'
-                        />
-                    </View>
+                   <Input
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                        inputStyle={styles.inputStyle}
+                        inputContainerStyle={styles.inputContainer}
+                        placeholder='Username'
+                        containerStyle={styles.inputOuterContainer}
+                    />
+
+                    <Input
+                        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                        inputStyle={styles.inputStyle}
+                        inputContainerStyle={styles.inputContainer}
+                        placeholder='Password'
+                        containerStyle={styles.inputOuterContainer}
+                    />
+
+                    <Text style={styles.text}>Forgotten password?</Text>
+
+                    <Button
+                        containerStyle={styles.buttonContainer}
+                        buttonStyle={styles.button}
+                        onPress={() => {
+                            Alert.alert('You tapped the button!');
+                        }}
+                        icon={
+                            <Icon
+                                name="check"
+                                size={20}
+                                color="white"
+                            />
+                        }
+                    />
+
+                </View>
             </ImageBackground>
         );
     }
@@ -97,24 +131,51 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         padding: 10
     },
-    loginText: {
-        color: 'tomato',
-        fontSize: 25,
-        letterSpacing: 1.2,
-        fontWeight: "bold",
+    inputs: {
+        width: '100%',
+        marginTop: '50%',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    inputContainer: {
+        backgroundColor: '#ffffff',
+        color: '#000000',
+        textAlign: 'center'
+    },
+    inputStyle: {
+        textAlign: 'center'
+    },
+    inputOuterContainer : {
+        borderRadius: 50,
+        width: '80%',
+        margin: 20,
+        textAlign: 'center'
+    },
+    buttonContainer : {
+        marginTop: '30%',
+        backgroundColor: 'tomato'
+    },
+    button : {
+        padding: 20,
+        backgroundColor: 'tomato'
+    },
+    text: {
+        color: 'tomato'
+    },
+    signUpContainer : {
+        marginTop: -80,
+        marginBottom: 80,
+        backgroundColor: 'tomato',
+        color: 'white'
+    },
+    signUpButton : {
         padding: 10,
-        marginTop: -50,
-        marginBottom: 20
+        backgroundColor: 'tomato'
     },
-    button: {
-        margin: 10,
-        padding: 30
-    },
-    googleButton: {
-        margin: 10,
-        padding: 30,
-        backgroundColor: Colors.COLOR_GRAY
+    icon: {
+        paddingRight: 5
     }
 });
 
 export default LoginScreen;
+

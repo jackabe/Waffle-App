3c5d297664bacb6b59377ed6313e1b8eb040db4b
Jack Allcock <jackallcock@yahoo.co.uk>
2019-02-16 14:48:23 +0000

    Add signup logic to signup modal

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, Alert} from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Alert, TouchableHighlight} from 'react-native';
 import {Header, Overlay} from "react-native-elements";
 import Colors from "../config/Colors";
 import Service from "../utils/Service";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button} from 'react-native-elements';
 import firebase from 'react-native-firebase';
+import DatePicker from 'react-native-datepicker'
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 class LoginScreen extends React.Component {
 
@@ -15,6 +17,9 @@ class LoginScreen extends React.Component {
             loading: true,
             email: '',
             password: '',
+            date: '',
+            firstName: '',
+            surname: '',
             showSignUp: false
         };
     }
@@ -35,6 +40,16 @@ class LoginScreen extends React.Component {
      */
     componentWillUnmount() {
         this.authSubscription();
+    };
+
+    checkInformation() {
+        const { firstName, surname, email, password, date } = this.state;
+        return !(firstName.length > 2 && surname.length > 2 && email.length > 2 && password.length > 2 && date.length > 2);
+    };
+
+    checkLogin() {
+        const {email, password} = this.state;
+        return !(email.length > 2 && password.length > 2);
     }
 
     login = () => {
@@ -59,13 +74,19 @@ class LoginScreen extends React.Component {
         })
     };
 
+    closeSignUpModal = () => {
+        Alert.alert('hi');
+        this.setState({
+            showSignUp: false
+        })
+    };
+
     onRegister = () => {
         const { email, password } = this.state;
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then((user) => {
-                // If you need to do anything with the user, do it here
-                // The user will be logged in automatically by the
-                // `onAuthStateChanged` listener we set up in App.js earlier
+                    Alert.alert('Logged in');
+                    this.props.navigation.navigate("App");
             })
             .catch((error) => {
                 const { code, message } = error;
@@ -73,7 +94,7 @@ class LoginScreen extends React.Component {
                 // The message contains the default Firebase string
                 // representation of the error
             });
-    }
+    };
 
     render() {
         return (
@@ -81,27 +102,108 @@ class LoginScreen extends React.Component {
                 source={{uri: 'https://www.pixelstalk.net/wp-content/uploads/images2/Minimalist-Abstract_arrows-wallpaper-2560x1600.jpg'}}
                 style={styles.container}
             >
+                {
+                    this.state.showSignUp && (
+                        <Overlay
+                            isVisible
+                            windowBackgroundColor="white"
+                            overlayBackgroundColor="white"
+                            fullScreen={true}>
+                            <TouchableHighlight style={styles.closeView} onPress={this.closeSignUpModal}>
+                                <View>
+                                    <Icon
+                                        name="close"
+                                        size={20}
+                                        color="tomato"
+                                    />
+                                </View>
+                            </TouchableHighlight>
 
-                <Overlay
-                    isVisible={this.state.isVisible}
-                    windowBackgroundColor="rgba(255, 255, 255, .5)"
-                    overlayBackgroundColor="red"
-                    width="auto"
-                    height="auto"><Text>Hello from Overlay!</Text></Overlay>;
-                
-                {/*<Overlay*/}
-                    {/*isVisible={this.state.isVisible}*/}
-                    {/*windowBackgroundColor="rgba(255, 255, 255, .5)"*/}
-                    {/*overlayBackgroundColor="red"*/}
-                    {/*width="auto"*/}
-                    {/*height="auto"*/}
-                {/*>*/}
-                    {/*<Text>Hello from Overlay!</Text>*/}
-                {/*</Overlay>;*/}
+                            <Text style={styles.signUpHeading}>Sign Up</Text>
+
+                            <View style={styles.signUpContainer}>
+
+                                <Input
+                                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                                    inputStyle={styles.inputStyle}
+                                    inputContainerStyle={styles.inputContainer}
+                                    placeholder='First name'
+                                    containerStyle={styles.inputOuterContainer}
+                                    onChangeText={(text) => this.setState({firstName: text})}
+                                />
+                                <Input
+                                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                                    inputStyle={styles.inputStyle}
+                                    inputContainerStyle={styles.inputContainer}
+                                    placeholder='Surname'
+                                    containerStyle={styles.inputOuterContainer}
+                                    onChangeText={(text) => this.setState({surname: text})}
+                                />
+                                <Input
+                                    leftIcon={{ type: 'Zocial', name: 'email' }}
+                                    inputStyle={styles.inputStyle}
+                                    inputContainerStyle={styles.inputContainer}
+                                    placeholder='Email'
+                                    containerStyle={styles.inputOuterContainer}
+                                    onChangeText={(text) => this.setState({email: text})}
+                                />
+                                <Input
+                                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                                    inputStyle={styles.inputStyle}
+                                    inputContainerStyle={styles.inputContainer}
+                                    placeholder='Password'
+                                    secureTextEntry={true}
+                                    containerStyle={styles.inputOuterContainer}
+                                    onChangeText={(text) => this.setState({password: text})}
+                                />
+                                <DatePicker
+                                    style={styles.inputOuterContainer}
+                                    date={this.state.date}
+                                    mode="date"
+                                    placeholder="Enter date of birth"
+                                    format="DD-MM-YYYY"
+                                    minDate="01-01-1900"
+                                    maxDate="01-01-2000"
+                                    confirmBtnText="Confirm"
+                                    cancelBtnText="Cancel"
+                                    customStyles={{
+                                        dateIcon: {
+                                            position: 'absolute',
+                                            right: 0,
+                                            top: 4,
+                                            marginLeft: 35
+                                        },
+                                        dateInput: {
+                                            marginLeft: 0
+                                        }
+                                        // ... You can check the source to find the other keys.
+                                    }}
+                                    onDateChange={(date) => {this.setState({date: date})}}
+                                />
+
+                                <Button
+                                    containerStyle={styles.signUpModalButtonContainer}
+                                    buttonStyle={styles.signUpModalButton}
+                                    disabled={this.checkInformation()}
+                                    onPress={() => {
+                                        this.onRegister();
+                                    }}
+                                    icon={
+                                        <Icon
+                                            name="check"
+                                            size={20}
+                                            color="white"
+                                        />
+                                    }
+                                />
+                            </View>
+                        </Overlay>
+                    )
+                }
 
                 <Header style={{borderBottomWidth: 0}}
                         barStyle="light-content"
-                        centerComponent={<Text style={styles.heading}>waffle</Text>}
+                        centerComponent={<Text style={styles.heading}>waffle </Text>}
                         containerStyle={{
                             width: '100%',
                             backgroundColor: 'transparent',
@@ -136,7 +238,7 @@ class LoginScreen extends React.Component {
                         leftIcon={{ type: 'font-awesome', name: 'user' }}
                         inputStyle={styles.inputStyle}
                         inputContainerStyle={styles.inputContainer}
-                        placeholder='Username'
+                        placeholder='Email'
                         containerStyle={styles.inputOuterContainer}
                         onChangeText={(text) => this.setState({email: text})}
                     />
@@ -156,6 +258,7 @@ class LoginScreen extends React.Component {
                     <Button
                         containerStyle={styles.buttonContainer}
                         buttonStyle={styles.button}
+                        disabled={this.checkLogin()}
                         onPress={() => {
                             this.login();
                         }}
@@ -222,6 +325,7 @@ const styles = StyleSheet.create({
     },
     inputStyle: {
         textAlign: 'center'
+
     },
     inputOuterContainer : {
         borderRadius: 50,
@@ -230,8 +334,9 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     buttonContainer : {
-        marginTop: '30%',
-        backgroundColor: 'tomato'
+        marginTop: '20%',
+        backgroundColor: 'tomato',
+        alignItems: 'center',
     },
     button : {
         padding: 20,
@@ -242,10 +347,9 @@ const styles = StyleSheet.create({
         color: 'tomato'
     },
     signUpContainer : {
-        marginTop: -80,
-        marginBottom: 80,
-        backgroundColor: 'tomato',
-        color: 'white'
+        width: '100%',
+        alignItems: 'center',
+        textAlign: 'center',
     },
     signUpButton : {
         padding: 10,
@@ -253,6 +357,30 @@ const styles = StyleSheet.create({
     },
     icon: {
         paddingRight: 5
+    },
+    signUpHeading: {
+        color: 'tomato',
+        fontSize: 28,
+        letterSpacing: 1.2,
+        fontWeight: "bold",
+        padding: 10,
+        marginBottom: 20
+    },
+    closeView : {
+        position: 'absolute',
+        right: 0,
+        margin: 20,
+        backgroundColor: 'white',
+        alignItems: 'center',
+    },
+    signUpModalButtonContainer : {
+        marginTop: '10%',
+        backgroundColor: 'tomato',
+        alignItems: 'center',
+    },
+    signUpModalButton : {
+        padding: 20,
+        backgroundColor: 'tomato'
     }
 });
 

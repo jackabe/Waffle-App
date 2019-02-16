677f44b82ee6622e1e1a2b00fb50e566c4df6d9e
Morgan Jones <JonesM125@cardiff.ac.uk>
2019-02-16 17:14:52 +0000

    Created Registration input field and date

--- src/screens/BookingScreen.js
+++ src/screens/BookingScreen.js
@@ -3,24 +3,77 @@ import {View, Text, StyleSheet} from 'react-native';
 import {Header, Overlay, Input, Button} from "react-native-elements";
 import headerStyling from "../styles/ui/Header";
 import ProfileHeaderButton from "../components/ProfileHeaderButton";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import DatePicker from "react-native-datepicker";
 
 class BookingScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
         return {
             headerStyle: headerStyling.headerStyle,
             headerTitleStyle: headerStyling.headerTitleStyle,
-            headerTitle: "waffle ",
+            headerTitle: "waffle  ",
             headerRight: <ProfileHeaderButton navigation={navigation}/>,
         };
     };
 
     constructor(props) {
         super(props);
+        this.state = {
+            // parkingLotId initialized at 1 until address finder feature completed
+            parkingLotId: '1',
+            date: '',
+            regNumber: '',
+            startTime: '',
+            endTime: '',
+        };
     }
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.bookingHeading}>Make a Booking</Text>
+
+                <View style={styles.bookingContainer}>
+
+                    <Text style={styles.parkingLotText}>Parking Lot Id: {this.state.parkingLotId}</Text>
+
+                    <Input
+                        leftIcon={{ type: 'font-awesome', name: 'car' }}
+                        inputStyle={styles.inputStyle}
+                        inputContainerStyle={styles.inputContainer}
+                        placeholder='Registration Number'
+                        containerStyle={styles.inputOuterContainer}
+                        onChangeText={(text) => this.setState({regNumber: text})}
+                    />
+
+                    <DatePicker
+                        style={styles.inputOuterContainer}
+                        date={this.state.date}
+                        mode="date"
+                        placeholder="Enter date of booking"
+                        format="DD-MM-YYYY"
+                        // May need to be updated so that the users can't book in past.
+                        minDate="01-01-1900"
+                        maxDate="01-01-2050"
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        customStyles={{
+                            dateIcon: {
+                                position: 'absolute',
+                                right: 0,
+                                top: 4,
+                                marginLeft: 35
+                            },
+                            dateInput: {
+                                marginLeft: 0
+                            }
+                            // ... You can check the source to find the other keys.
+                        }}
+                        onDateChange={(date) => {this.setState({date: date})}}
+                    />
+
+
+                </View>
+
             </View>
         );
     }
@@ -92,7 +145,7 @@ const styles = StyleSheet.create({
         paddingTop: 20,
         color: 'tomato'
     },
-    signUpContainer : {
+    bookingContainer : {
         width: '100%',
         alignItems: 'center',
         textAlign: 'center',
@@ -109,6 +162,12 @@ const styles = StyleSheet.create({
         fontSize: 28,
         letterSpacing: 1.2,
         fontWeight: "bold",
+        padding: 10,
+    },
+    parkingLotText: {
+        color: 'tomato',
+        fontSize: 20,
+        letterSpacing: 1.2,
         padding: 10,
     },
     closeView : {

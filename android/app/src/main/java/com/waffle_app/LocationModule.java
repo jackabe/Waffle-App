b3ed9c9a2b4c68557786482b2080b744b781c55d
Jack Allcock <jackallcock@yahoo.co.uk>
2019-03-03 22:00:58 +0000

    Run geofence as background task

--- android/app/src/main/java/com/waffle_app/LocationModule.java
+++ android/app/src/main/java/com/waffle_app/LocationModule.java
@@ -7,6 +7,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
@@ -19,9 +26,9 @@ import java.util.Map;
 
 public class LocationModule extends ReactContextBaseJavaModule {
 
-    private GeofencingClient mGeofencingClient;  // Provides access to the Geofencing API.
     private ArrayList<Geofence> mGeofenceList; // The list of geofences used in this sample.
     private PendingIntent mGeofencePendingIntent; // Used when requesting to add or remove geofences.
+    FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(getReactApplicationContext()));
 
     public LocationModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -35,89 +42,32 @@ public class LocationModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void startScanning(Promise promise) {
 
-        // Initialise array list
-        mGeofenceList = new ArrayList<>();
-        // Create all of the geofences
-        populateGeofenceList();
-        // Initially set the PendingIntent used in addGeofences() and removeGeofences() to null.
-        mGeofencePendingIntent = null;
-        // Initialise the geofencing client
-        mGeofencingClient = LocationServices.getGeofencingClient(getCurrentActivity());
+        Job myJob = dispatcher.newJobBuilder()
+                // the JobService that will be called
+                .setService(LocationJobService.class)
+                // uniquely identifies the job
+                .setTag("my-unique-tag")
+                // one-off job
+                .setRecurring(false)
+                // don't persist past a device reboot
+                .setLifetime(Lifetime.FOREVER)
+                // start between 0 and 60 seconds from now
+                .setTrigger(Trigger.executionWindow(0, 60))
+                // don't overwrite an existing job with the same tag
+                .setReplaceCurrent(true)
+                // retry with exponential backoff
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                // constraints that need to be satisfied for the job to run
+//                .setConstraints(
+//                        // only run on an unmetered network
+//                        Constraint.ON_UNMETERED_NETWORK,
+//                        // only run when the device is charging
+//                        Constraint.DEVICE_CHARGING
+//                )
+                .build();
 
-        // Start the service
-        startGeofenceService();
+        dispatcher.mustSchedule(myJob);
 
-    }
-
-    /**
-     * Currently creates list of geofences hard coded
-     * TODO Dynamically create geofences based on the user's location.
-     */
-    private void populateGeofenceList() {
-        for (Map.Entry<String, LatLng> entry : Constants.FENCES.entrySet()) {
-            mGeofenceList.add(new Geofence.Builder()
-                    // Give request an ID
-                    .setRequestId(entry.getKey()) // This is a string to identify this geofence.
-                    // Set the circular region of this geofence.
-                    .setCircularRegion(
-                            entry.getValue().latitude,
-                            entry.getValue().longitude,
-                            Constants.GEOFENCE_RADIUS_IN_METERS
-                    )
-                    // Set the expiration duration of the geofence. This geofence gets automatically
-                    // removed after this period of time.
-                    .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
-                    // Set the transition types of interest. Alerts only made for enter/exit
-                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_DWELL)
-                    .setLoiteringDelay(10000)
-                    // Build the geofence.
-                    .build());
-        }
-    }
-
-    /**
-     * Adds geofences, which sets alerts to be notified when the device enters or exits one of the
-     * specified geofences. Handles the success or failure results returned by addGeofences().
-     */
-    public void startGeofenceService() {
-        try {
-            mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent());
-        } catch (SecurityException e) {
-        }
-    }
-
-    /**
-     * Builds and returns a GeofencingRequest. Specifies the list of geofences to be monitored.
-     * Also specifies how the geofence notifications are initially triggered.
-     */
-    private GeofencingRequest getGeofencingRequest() {
-        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
-        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
-        // is already inside that geofence.
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
-        // Add the geofences to be monitored by geofencing service.
-        builder.addGeofences(mGeofenceList);
-        // Return a GeofencingRequest.
-        return builder.build();
-    }
-
-
-    /**
-     * Gets a PendingIntent to send with the request to add or remove Geofences. Location Services
-     * issues the Intent inside this PendingIntent whenever a geofence transition occurs for the
-     * current list of geofences.
-     *
-     * @return A PendingIntent for the IntentService that handles geofence transitions.
-     */
-    private PendingIntent getGeofencePendingIntent() {
-        // Reuse the PendingIntent if we already have it.
-        if (mGeofencePendingIntent != null) {
-            return mGeofencePendingIntent;
-        }
-        Intent intent = new Intent(getCurrentActivity(), GeofenceBroadcastReceiver.class);
-        mGeofencePendingIntent = PendingIntent.getBroadcast(getCurrentActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        return mGeofencePendingIntent;
     }
 
 }

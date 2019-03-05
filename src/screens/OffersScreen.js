31e0de161748b740332a7716ef796b2a27433df7
Mert Gol <GolMA@cardiff.ac.uk>
2019-03-05 11:22:37 +0000

    Initial commit

--- src/screens/OffersScreen.js
+++ src/screens/OffersScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import headerStyling from "../styles/ui/Header";
 import ProfileHeaderButton from "../components/ProfileHeaderButton";
+import firebase from "react-native-firebase";
+import {Input, ListItem} from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 class OffersScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -12,13 +15,288 @@ class OffersScreen extends React.Component {
             headerRight: <ProfileHeaderButton navigation={navigation}/>,
         };
     };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId:null,
+            offerId:null,
+            offerList: [
+                {
+                    company:'McDonalds',
+                    offer:'Free Cheeseburger',
+                    expiry:'15 March 2019'
+                },
+
+                {
+                    company:'Subway',
+                    offer:'Free Drink',
+                    expiry:'15 April 2019'
+                },
+
+                {
+                    company:'Burger King',
+                    offer:'Free Side',
+                    expiry:'28 March 2019'
+                },
+            ]
+
+        }
+    }
+
+    componentDidMount() {
+        this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({
+                    userId: user.uid
+                })
+            }
+        });
+    }
+
+
+    getColour(company){
+        const s_styles = StyleSheet.create({
+            available: {
+                paddingTop: 10,
+                fontWeight: 'bold',
+                color: 'white'
+            },
+            price: {
+                paddingTop: 10,
+                fontWeight: 'bold',
+                color: 'white'
+            },
+            carParkTitle: {
+                fontWeight: 'bold',
+                color: 'white',
+                fontSize: 15
+            },
+            subtitleStyle: {
+                color: 'white',
+            },
+            subtitleView: {
+                color: 'white',
+                backgroundColor: 'green'
+            },
+            listContainer: {
+                width: '100%',
+            },
+            listContentContainer: {
+                backgroundColor: 'green',
+                padding: 20,
+                paddingBottom: 35
+            },
+        });
+        const m_styles = StyleSheet.create({
+            available: {
+                paddingTop: 10,
+                fontWeight: 'bold',
+                color: 'white'
+            },
+            price: {
+                paddingTop: 10,
+                fontWeight: 'bold',
+                color: 'white'
+            },
+            carParkTitle: {
+                fontWeight: 'bold',
+                color: 'white',
+                fontSize: 15
+            },
+            subtitleStyle: {
+                color: 'white',
+            },
+            subtitleView: {
+                color: 'white',
+                backgroundColor: 'red'
+            },
+            listContainer: {
+                width: '100%',
+            },
+            listContentContainer: {
+                backgroundColor: 'red',
+                padding: 20,
+                paddingBottom: 35
+            },
+        });
+        const b_styles = StyleSheet.create({
+            available: {
+                paddingTop: 10,
+                fontWeight: 'bold',
+                color: 'white'
+            },
+            price: {
+                paddingTop: 10,
+                fontWeight: 'bold',
+                color: 'white'
+            },
+            carParkTitle: {
+                fontWeight: 'bold',
+                color: 'white',
+                fontSize: 15
+            },
+            subtitleStyle: {
+                color: 'white',
+            },
+            subtitleView: {
+                color: 'white',
+                backgroundColor: 'red'
+            },
+            listContainer: {
+                width: '100%',
+            },
+            listContentContainer: {
+                backgroundColor: 'red',
+                padding: 20,
+                paddingBottom: 35
+            },
+        });
+
+        var colours = {
+            'McDonalds': m_styles,
+            'Subway': s_styles,
+            'Burger King': b_styles
+        };
+
+        try {
+            return colours[company]
+        } catch (e) {
+            return "Company Doesn\'t Exist"
+        }
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Offers Screen</Text>
+            <View style={styles.content}>
+                {
+                    this.state.offerList.map((l, i) => (
+                        <ListItem
+                            containerStyle={this.getColour(l.company).listContainer}
+                            contentContainerStyle={this.getColour(l.company).listContentContainer}
+                            titleStyle={this.getColour(l.company).carParkTitle}
+                            subtitleStyle={this.getColour(l.company).subtitleStyle}
+                            key={i}
+
+                            //on press commented out until offers are created dynamically
+                            // onPress={() => {
+                            //     do this on press
+                            // }}
+                            title={l.company}
+                            subtitle={
+                                <View style={styles.subtitleView}>
+                                    {l.favourite ?
+                                        <Icon
+                                            style={styles.icon}
+                                            name="star"
+                                            size={25}
+                                            color="white"
+                                        /> : null }
+                                    <Text style={styles.available}>{'Offer: ' + l.offer }</Text>
+                                    <Text style={styles.price}>{'This offer will expire on: ' + l.expiry}</Text>
+                                </View>
+                            }
+                            // leftAvatar={{ source: require('../images/avatar1.jpg') }}
+                        />
+                    ))
+                }
             </View>
         );
     }
 }
 
 export default OffersScreen;
+
+
+const styles = StyleSheet.create({
+    available: {
+        paddingTop: 10,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    price: {
+        paddingTop: 10,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    carParkTitle: {
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 15
+    },
+    subtitleStyle: {
+        color: 'white',
+    },
+    subtitleView: {
+        color: 'white',
+        backgroundColor: 'tomato'
+    },
+    listContainer: {
+        width: '100%',
+    },
+    listContentContainer: {
+        backgroundColor: 'tomato',
+        padding: 20,
+        paddingBottom: 35
+    },
+    listContentContainerMcDonalds: {
+        backgroundColor: 'red',
+        padding: 20,
+        paddingBottom: 35
+    },
+    content: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        textAlign: 'center',
+        paddingTop: 30,
+    },
+    inputs: {
+        width: '100%',
+        marginTop: '50%',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    inputContainer: {
+        backgroundColor: '#ffffff',
+        borderBottomWidth: 0
+    },
+    inputStyle: {
+        textAlign: 'center',
+        paddingLeft: 10,
+        fontSize: 15
+    },
+    inputOuterContainer : {
+        borderColor: 'grey',
+        borderWidth: 1,
+        width: '80%',
+        margin: 20,
+    },
+    buttonContainer : {
+        marginTop: '20%',
+        backgroundColor: 'tomato',
+        alignItems: 'center',
+    },
+    button : {
+        padding: 20,
+        backgroundColor: 'tomato'
+    },
+    createAccount: {
+        paddingTop: 20,
+        color: 'tomato'
+    },
+    signUpContainer : {
+        width: '100%',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    signUpButton : {
+        padding: 10,
+        backgroundColor: 'tomato'
+    },
+    icon: {
+        right: 0,
+        marginTop: -25,
+        position: 'absolute'
+    },
+});

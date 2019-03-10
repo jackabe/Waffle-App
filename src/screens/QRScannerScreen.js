3b303e3083936e5b3b1a6a85709493a21dd18c7a
Morgan Jones <JonesM125@cardiff.ac.uk>
2019-03-10 21:03:06 +0000

    Styling for scanner

--- src/screens/QRScannerScreen.js
+++ src/screens/QRScannerScreen.js
@@ -37,16 +37,6 @@ class ScannerScreen extends React.Component {
 
                 <QRCodeScanner
                     onRead={this.onSuccess.bind(this)}
-                    topContent={
-                        <Text style={styles.centerText}>
-                            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-                        </Text>
-                    }
-                    bottomContent={
-                        <TouchableOpacity style={styles.buttonTouchable}>
-                            <Text style={styles.buttonText}>OK. Got it!</Text>
-                        </TouchableOpacity>
-                    }
                 />
 
             </View>

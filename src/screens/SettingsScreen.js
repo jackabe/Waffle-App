7358bab0ba517deaca7d5249c85a15198e9cb3d6
Morgan Jones <JonesM125@cardiff.ac.uk>
2019-03-10 13:32:10 +0000

    Added styles from style-sheet

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -11,6 +11,17 @@ class SettingsScreen extends React.Component {
             headerRight: <Text style={styles.account}>Settings</Text>,
         };
     };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: null,
+            newPassword: "",
+            currentPassword: "",
+        }
+    }
+
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -28,6 +39,30 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginRight: 25
     },
+    heading: {
+        color: 'tomato',
+        fontSize: 35,
+        fontWeight: "bold",
+        padding: 10,
+    },
+    inputContainer: {
+        backgroundColor: '#ffffff',
+        borderBottomWidth: 0
+    },
+    inputStyle: {
+        textAlign: 'center',
+        paddingLeft: 10,
+        fontSize: 15
+    },
+    inputOuterContainer : {
+        borderColor: 'grey',
+        borderWidth: 1,
+        width: '80%',
+        margin: 20,
+    },
+
+
+
 });
 
 export default SettingsScreen;
